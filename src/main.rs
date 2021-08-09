274cb15775b159eb39d22020f0c37031458fb4a1
Kitamura <ymyk6602@gmail.com>
2021-08-09 18:47:19 +0900

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use image::{DynamicImage, GenericImageView, ImageBuffer};
 use viewer;
 
 fn main() {
     env_logger::init();
     println!("Hello world!");
-    viewer::hello_from_viewer();
-
-    let app = viewer::app::App::new(1280, 960).unwrap();
 
     let image_file = "/home/kitamura/work/Projects/improc/data/sample_image/surface.png";
     let mut image = image::open(image_file).unwrap();
-    image = image.flipv();
 
+    let app = viewer::app::App::new(1280, 960).unwrap();
+    image = image.flipv();
     app.add_image(&image, "default")
         .add_point("default", 0.0f32, 0.0f32, 0.6f32, 1.0f32, 0.0f32, 0.0f32)
         .add_point("default", 1.0f32, 0.0f32, 0.6f32, 1.0f32, 0.0f32, 0.0f32)
