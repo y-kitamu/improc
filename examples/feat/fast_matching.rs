64bd2733a60254672bb5fcc332da137091eb501c
Kitamura <ymyk6602@gmail.com>
2021-08-15 22:15:28 +0900

    update example

--- examples/feat/fast_matching.rs
+++ examples/feat/fast_matching.rs
@@ -1,1 +1,15 @@
+use std::thread;
 
+struct Sample {
+    vec: Vec<u32>,
+}
+
+impl Sample {
+    fn sample(&mut self) {
+        self.vec.push(10);
+    }
+}
+
+fn main() {
+    let mut sample = Sample { vec: Vec::new() };
+}
