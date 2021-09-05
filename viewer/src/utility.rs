a2a7f699a4aab26bcfd0499373a5c26c6b9ed2a1
Kitamura <ymyk6602@gmail.com>
2021-09-05 15:13:39 +0900

    add tests

--- viewer/src/utility.rs
+++ viewer/src/utility.rs
@@ -2,7 +2,7 @@ use std::ops::Deref;
 
 use cgmath::num_traits::ToPrimitive;
 use image::{ImageBuffer, Pixel, RgbImage};
-use sdl2::sys::{SDL_GL_GetCurrentWindow, SDL_GetMouseState, SDL_GetWindowSize};
+use sdl2::sys::SDL_GetMouseState;
 
 use crate::Matrix4;
 
@@ -31,13 +31,6 @@ pub fn get_mouse_pos() -> (i32, i32) {
     (x, y)
 }
 
-pub fn get_window_size() -> (f32, f32) {
-    let mut x = 0;
-    let mut y = 0;
-    unsafe { SDL_GetWindowSize(SDL_GL_GetCurrentWindow(), &mut x, &mut y) };
-    (x as f32, y as f32)
-}
-
 pub fn scale_matrix(model_mat: &Matrix4, cx: f32, cy: f32, scale: f32) -> Matrix4 {
     let mut dst = model_mat.clone();
     let mut x = (cx - model_mat[3][0]) / (model_mat[0][0] + 1e-9);
@@ -53,4 +46,47 @@ pub fn scale_matrix(model_mat: &Matrix4, cx: f32, cy: f32, scale: f32) -> Matrix
     dst[0][0] *= scale;
     dst[1][1] *= scale;
     dst
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cgmath::One;
+
+    #[test]
+    fn test_convert_to_rgb() {
+        let length = 10;
+        let gray = image::GrayImage::from_fn(length, length, |x, y| image::Luma([(x + y) as u8]));
+        let color = convert_to_rgb(&gray);
+        assert_eq!(color.len() as u32, length * length * 3);
+
+        for y in 0..length {
+            for x in 0..length {
+                let p = color.get_pixel(x, y);
+                assert_eq!(p.0[0], (x + y) as u8);
+                assert_eq!(p.0[0], p.0[1]);
+                assert_eq!(p.0[1], p.0[2]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_scale_matrix() {
+        let mat = Matrix4::one();
+        let cx = 10.0;
+        let cy = -10.0;
+        let scale = 1.5;
+
+        let dst = scale_matrix(&mat, cx, cy, scale);
+        assert!((dst[0][0] - 1.5).abs() < 1e-5, "dst[0][1] = {}", dst[0][0]);
+        assert!((dst[1][1] - 1.5).abs() < 1e-5, "dst[1][1] = {}", dst[1][1]);
+        assert!((dst[3][0] - 0.0).abs() < 1e-5, "dst[3][0] = {}", dst[3][0]);
+        assert!((dst[3][1] - 0.0).abs() < 1e-5, "dst[3][1] = {}", dst[3][1]);
+
+        let dst = scale_matrix(&dst, 0.75, -0.3, 2.0);
+        assert!((dst[0][0] - 3.0).abs() < 1e-5, "dst[0][1] = {}", dst[0][0]);
+        assert!((dst[1][1] - 3.0).abs() < 1e-5, "dst[1][1] = {}", dst[1][1]);
+        assert!((dst[3][0] + 0.75).abs() < 1e-5, "dst[3][0] = {}", dst[3][0]);
+        assert!((dst[3][1] - 0.30).abs() < 1e-5, "dst[3][1] = {}", dst[3][1]);
+    }
 }
