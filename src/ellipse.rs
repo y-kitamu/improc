a2b248087f44c9709b1587b98091e89073cc8dff
Kitamura <ymyk6602@gmail.com>
2022-03-22 22:29:36 +0900

    add renormalization method (impl & test)

--- src/ellipse.rs
+++ src/ellipse.rs
@@ -2,5 +2,6 @@
 pub mod iterative_reweight;
 pub mod least_square;
 pub mod matrix_ops;
+pub mod renormalization;
 pub mod taubin;
 pub mod test_utility;
--- src/ellipse/renormalization.rs
+++ src/ellipse/renormalization.rs
@@ -1,17 +1,22 @@
-//! Functions for fitting data points to ellipse using Iterative reweight method.
+//! Implementation of re-normalization method.
+//! Re-normalization method is algorithm for fitting data points to ellipse.
 use anyhow::Result;
 use nalgebra as na;
 
-use super::least_square::{least_square_fitting, least_square_fitting_with_weight};
+use super::{
+    iterative_reweight::calc_ellipse_var_mat,
+    taubin::{taubin, taubin_with_weight},
+};
 
-pub fn iterative_reweight(
+pub fn renormalization(
     data: &[na::Point2<f64>],
     threshold: f64,
     max_iterate: usize,
 ) -> Result<na::DVector<f64>> {
-    let mut params = least_square_fitting(data, 1.0)?;
+    let mut params = taubin(data, 1.0)?;
     let mut previous: na::DVector<f64> =
         na::DVector::<f64>::from_iterator(params.len(), (0..params.len()).map(|_| 0.0));
+
     for _ in 1..max_iterate {
         if previous[0] * params[0] < 0.0 {
             previous *= -1.0;
@@ -27,24 +32,9 @@ pub fn iterative_reweight(
             })
             .collect::<Vec<f64>>();
         previous = params.clone();
-        params = least_square_fitting_with_weight(data, 1.0, &weight).unwrap();
+        params = taubin_with_weight(data, 1.0, &weight).unwrap();
     }
     Ok(params)
-}
-
-pub fn calc_ellipse_var_mat(pt: &na::Point2<f64>) -> na::Matrix6<f64> {
-    let x = pt[0];
-    let y = pt[1];
-    #[rustfmt::skip]
-    let var_mat = na::Matrix6::<f64>::from_column_slice(&[
-                    x * x, x * y, 0.0, x, 0.0, 0.0,
-                    x * y, x * x + y * y, x * y, y, x, 0.0,
-                    0.0, x * y, y * y, 0.0, y , 0.0,
-                    x, y, 0.0, 1.0, 0.0, 0.0,
-                    0.0, x, y, 0.0, 1.0, 0.0,
-                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ]);
-    4.0 * var_mat
 }
 
 #[cfg(test)]
@@ -55,7 +45,7 @@ mod tests {
     use rand::prelude::*;
 
     #[test]
-    fn test_iterative_reweight() {
+    fn test_renormalization() {
         // x^2 + y^2 - 1 = 0
         let ans = normalize(&[1.0, 0.0, 1.0, 0.0, 0.0, -1.0]);
         let std_dev = 0.05;
@@ -69,7 +59,8 @@ mod tests {
             })
             .collect();
 
-        let pred = iterative_reweight(&points, 1e-7, 100).unwrap();
-        compare_vecs_without_sign(&ans, pred.as_slice(), 1e-2);
+        let pred = renormalization(&points, 1e-7, 100).unwrap();
+        let normed = normalize(pred.as_slice());
+        compare_vecs_without_sign(&ans, &normed, 1e-2);
     }
 }
