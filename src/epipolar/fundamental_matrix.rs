c3924c134af1b9e4e72fc5b9ad2fbb8bbaa6e6f7
Kitamura <ymyk6602@gmail.com>
2022-03-27 19:10:28 +0900

    wip fundamental matrix optimization

--- src/epipolar/fundamental_matrix.rs
+++ src/epipolar/fundamental_matrix.rs
@@ -2,6 +2,39 @@
 use anyhow::Result;
 use nalgebra as na;
 
+use crate::optimizer::ObservedData;
+
+struct FundamentalMatrixData<'a> {
+    data: &'a [na::Point2<f64>],
+    scale: f64,
+}
+
+impl<'a> ObservedData<'a> for FundamentalMatrixData<'a> {
+    fn new(data: &'a [na::Point2<f64>]) -> Self {
+        todo!()
+    }
+
+    fn len(&self) -> usize {
+        self.data[0].len()
+    }
+
+    fn vector(&self, data_index: usize) -> na::DVector<f64> {
+        todo!()
+    }
+
+    fn matrix(&self, weight_vector: &[f64]) -> na::DMatrix<f64> {
+        todo!()
+    }
+
+    fn variance(&self, data_index: usize) -> na::DMatrix<f64> {
+        todo!()
+    }
+
+    fn weights(&self, params: &na::DVector<f64>) -> Vec<f64> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
