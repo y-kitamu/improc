aa15a28f6de45656d0df8ed7a59eca1621f1266b
Kitamura <ymyk6602@gmail.com>
2022-05-19 21:51:02 +0900

    add sample code

--- examples/feat/fast_detect.rs
+++ examples/feat/fast_detect.rs
@@ -1,9 +1,12 @@
 use std::path::Path;
 
+use nalgebra as na;
+
 use image::GenericImageView;
 use improc::{
     feat::keypoints::{fast::FASTCornerDetector, KeypointDetector},
     imgproc::gray,
+    json_writer::ViewerWriter,
 };
 
 fn main() {
@@ -22,4 +25,19 @@ fn main() {
 
     let fast = FASTCornerDetector::new(3, (50 * 50) as f32, 1, 1.0, true);
     let feats = fast.detect(&gray, 0);
+
+    let cur_file = Path::new(file!());
+    let output_path = cur_file
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("output/sample_feat.json");
+    let mut writer = ViewerWriter::new(output_path.to_str().unwrap());
+    writer.add_points(&feats, &na::Vector3::from_vec(vec![1.0, 0.0, 0.0]));
+    let output_str = writer.flush().unwrap();
+    println!("Output json : {}", output_path.to_str().unwrap());
+    println!("{}", output_str);
 }
