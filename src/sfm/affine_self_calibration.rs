3fd04680bf4304f33db069a1c097eeaa7d0d4b70
Kitamura <ymyk6602@gmail.com>
2022-05-19 21:22:55 +0900

    update affine self calibration (wip)

--- src/sfm/affine_self_calibration.rs
+++ src/sfm/affine_self_calibration.rs
@@ -1,8 +1,10 @@
 use anyhow::{ensure, Context, Result};
 use nalgebra as na;
 
+use crate::linalg::get_zero_mat;
+
 /// Self calibration using affine camera model.
-///
+/// - observed_pts : Observed points. (2d vector : [index of camera][index of point])
 /// - Return : Tuple of (motion matrix (stacked camera matrices), shape matrix (stacked 3d points)).
 pub fn affine_self_calibration(
     observed_pts: &[Vec<na::Point2<f64>>],
@@ -12,8 +14,8 @@ pub fn affine_self_calibration(
     let n_cameras = observed_pts[0].len();
 
     let observation_matrix: na::DMatrix<f64> = na::DMatrix::from_row_slice(
+        2 * n_cameras,
         n_points,
-        n_cameras,
         &observed_pts
             .iter()
             .map(|pts| {
@@ -27,6 +29,39 @@ pub fn affine_self_calibration(
     let (motion_mat, shape_mat) = calc_motion_and_shape_matrix(&observation_matrix)?;
 
     // calculate metric condition (affine reconstruction)
+    let bs: na::DMatrix<f64> = (0..n_cameras).fold(get_zero_mat(9), |acc, idx| {
+        let (mut tx, mut ty) = observed_pts.iter().fold((0.0, 0.0), |(xsum, ysum), pts| {
+            (xsum + pts[idx].x, ysum + pts[idx].y)
+        });
+        tx /= n_points as f64;
+        ty /= n_points as f64;
+        let a = tx * ty;
+        let c = tx * tx - ty * ty;
+
+        let u1 = motion_mat.row(idx * 2);
+        let u2 = motion_mat.row(idx * 2 + 1);
+        let u1mat = na::DMatrix::from_rows(&[u1, u1, u1]);
+        let u2mat = na::DMatrix::from_rows(&[u2, u2, u2]);
+
+        let coeffs: na::DMatrix<f64> = a * u1mat.component_mul(&u1mat.transpose())
+            - c * u1mat.component_mul(&u2mat.transpose())
+            - a * u2mat.component_mul(&u2mat.transpose());
+        let b = na::DMatrix::from_fn(9, 9, |r, c| coeffs[(r / 3, r % 3)] * coeffs[(c / 3, c % 3)]);
+        acc + b
+    });
+
+    let r2 = 2.0f64.sqrt();
+    #[rustfmt::skip]
+    let bmat = na::DMatrix::from_row_slice(6, 6, &[
+        bs[(0, 0)], bs[(0, 4)], bs[(0, 8)], r2 * bs[(0, 5)], r2 * bs[(0, 6)], r2 * bs[(0, 1)],
+        bs[(4, 0)], bs[(4, 4)], bs[(4, 8)], r2 * bs[(4, 5)], r2 * bs[(4, 6)], r2 * bs[(4, 1)],
+        bs[(8, 0)], bs[(8, 4)], bs[(8, 8)], r2 * bs[(8, 5)], r2 * bs[(8, 6)], r2 * bs[(8, 1)],
+        r2 * bs[(5, 0)], r2 * bs[(5, 4)], r2 * bs[(5, 8)], bs[(5, 5)], bs[(5, 6)], bs[(5, 1)],
+        r2 * bs[(6, 0)], r2 * bs[(6, 4)], r2 * bs[(6, 8)], bs[(6, 5)], bs[(6, 6)], bs[(6, 1)],
+        r2 * bs[(1, 0)], r2 * bs[(1, 4)], r2 * bs[(1, 8)], bs[(1, 5)], bs[(1, 6)], bs[(1, 1)],
+    ]);
+
+    // constrained least square method
 
     Ok((motion_mat, shape_mat))
 }
