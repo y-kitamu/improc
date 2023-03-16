eae3cdfac461c6a45e622bf6bf8f0eb337a170de
Kitamura <ymyk6602@gmail.com>
2023-03-16 22:52:38 +0900

    wip

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { ReactEventHandler, useState } from "react";
+import React, { ReactEventHandler, Suspense, useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { dialog } from "@tauri-apps/api";
 import { Button } from "@mui/material";
 import * as THREE from "three";
@@ -10,9 +10,62 @@ type Image = {
   data: number[];
 };
 
+type ImageProps = {
+  selected: string[];
+};
+
+const Image = (props: ImageProps) => {
+  const { selected } = props;
+  const [map, setMap] = useState<THREE.texture>();
+
+  useEffect(() => {
+    const loadTexture = async () => {
+      if (selected.length === 0) {
+        return;
+      }
+      const allImages: Image[] = await Promise.all(
+        selected.map(async (path: string) => {
+          return await invoke("read_image", { path: path });
+        })
+      );
+
+      const image = allImages[0];
+      const texture = new THREE.DataArrayTexture(
+        image.data,
+        image.size[0],
+        image.size[1],
+        1
+      );
+      setMap(texture);
+    };
+
+    const texture = useLoader(THREE.TextureLoader, selected[0]);
+    setMap(texture);
+  }, [selected]);
+
+  console.log("map : ", map);
+  // <ambientLight intensity={0.1} />
+  // <directionalLight color="red" position={[0, 0, 5]} />
+  // <sprite position={[1, 1, 0]}>
+  //   {{ map } && <spriteMaterial map={map} />}
+  // </sprite>
+  // <mesh position={[0, 0, 0]}>
+  //   <sphereGeometry args={[3, 3, 3]} />
+  //   <meshBasicMaterial color="blue" map={map} />
+  // </mesh>
+
+  return (
+    <>
+      <Suspense fallback={null}>
+        {map && <primitive object={map.scene} />}
+      </Suspense>
+    </>
+  );
+};
+
 const App = () => {
   const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [filelist, setFilelist] = useState<string[]>([]);
 
   const greet = async () => {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -36,20 +89,9 @@ const App = () => {
       if (selected === null) {
         console.log("no file selected");
         return;
+      } else if (Array.isArray(selected)) {
+        setFilelist(selected);
       }
-      const allImages: Image[] = Promise.all(
-        selected.map(async (path) => {
-          return await invoke("read_image", { path: path });
-        })
-      );
-
-      const image = allImages[0];
-      const texture = THREE.DataArrayTexture(
-        image.data,
-        image.size[0],
-        image.size[1],
-        1
-      );
     })();
   };
 
@@ -60,16 +102,7 @@ const App = () => {
         Open by mui
       </Button>
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh position={[2, 2, 0]}>
-          <boxGeometry args={[2, 2, 2]} />
-          <shaderMaterial />
-        </mesh>
-        <mesh position={[-2, -2, 0]}>
-          <sphereGeometry args={[1, 16, 16]} />
-          <meshBasicMaterial color="blue" />
-        </mesh>
+        <Image selected={filelist} />
       </Canvas>
     </div>
   );
